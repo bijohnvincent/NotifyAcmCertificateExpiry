77f53834d2f3758b753bce0778b15dacff9b9e39
Bijohn Vincent <bijohnvincent@users.noreply.github.com>
2020-06-22 19:10:01 +0300

    Added Slack notification

--- NotifyCertificateExpiry.py
+++ NotifyCertificateExpiry.py
@@ -1,22 +1,20 @@
 # Name          : notifyCertificateExpiry
-# Author        : Bijohn Vincent
+# Author        : github.com/bijohnvincent
 # Functionality : This function is for notifying when a certificate imported in ACM is about to expire
 # Tested and working fine on: AWS Lambda with python 3.6 + boto3
 
-#Import modules
-import boto3, datetime
-
-########################### Add your SNS ARN ################################
-SnsTopicArn = "SNS ARN"
-#############################################################################
+#
+#from botocore.vendored import requests
+import boto3, datetime,  json, os, urllib3
+from base64 import b64decode
 
 # Enter number of days before you need notification about certificate expiry.
-notifyIfExpiresAfter = 40
+notifyIfExpiresAfter = int(os.environ['notifyIfExpiresAfter'])
 
 # Set 'True' Only if you need notification on everytime this function runs. 
 # If false function will notify only when your certificate is about to expire
 # Idealy should be Flase
-notifyEveryTime = False
+notifyEveryTime = eval(os.environ['notifyEveryTime'])
 
 
 # Main function
@@ -33,9 +31,9 @@ def lambda_handler(event, context):
     
     for certificate in certificateList:
         certificateDetails = acmClient.describe_certificate(CertificateArn=certificate["CertificateArn"])["Certificate"]
+        #print (certificateDetails)
         certificateExpiresIn = certificateDetails['NotAfter'].replace(tzinfo=None) - now
-        
-            
+
         if certificateExpiresIn <= datetime.timedelta(days = notifyIfExpiresAfter):
             message += "\nCertificate with SAN " +repr(certificateDetails['SubjectAlternativeNames']) + \
             " expires in " + repr(certificateExpiresIn.days) + " days"
@@ -53,15 +51,36 @@ def lambda_handler(event, context):
     else:
         snsSubject = 'AWS Certificate Manager update'
     
-    # Send notification using SNS topic
+    # Send notification
     if notify or notifyEveryTime:
+        # Send notification using SNS topic
         SnsClient = boto3.client('sns')
         response = SnsClient.publish(
-            TopicArn= SnsTopicArn,
+            TopicArn= os.environ['SnsTopicArn'],
             Message= message,
             Subject= snsSubject,
             MessageStructure='string'
             )
+            
+        # If Slack web hook URL varible is provided, send notification to slack channel
+        if os.environ['SlackWebhookUrl']:
+            print ("send Slack notification")
+            slack_data = {'text': message}
+            EncryptedSlackWebHookUrl = os.environ['SlackWebhookUrl']
+            DecryptedSlackWebHookUrl = boto3.client('kms').decrypt(CiphertextBlob=b64decode(EncryptedSlackWebHookUrl))['Plaintext'].decode('utf-8')
+            
+            http = urllib3.PoolManager()
+            response = http.request('POST',
+                DecryptedSlackWebHookUrl, 
+                body=json.dumps(slack_data),
+                headers={'Content-Type': 'application/json'}
+            )
+            if response.status != 200:
+                raise ValueError(
+                    'Request to slack returned an error %s, the response is:\n%s'
+                    % (response.status, response.data.decode('utf-8'))
+                )
     else:
         message += "\nNo certificates expiring in " + repr (notifyIfExpiresAfter) + " days."
+            
     print (message)
